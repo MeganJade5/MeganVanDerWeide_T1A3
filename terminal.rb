f7bf005702cee14858022e9486aa5988dd2e7c6f
Megan Van Der Weide <meganvanderweide@Megans-MacBook-Air.local>
2022-03-25 16:35:55 +1100

    loop updates to menu

--- terminal.rb
+++ terminal.rb
@@ -1,35 +1,42 @@
+def trainermenu
 # hello message
-while exit == false
-    puts "Hello Trainer\nWhat would you like to do today?"
+    puts 'Hello Trainer'
+    exit = false
+    while exit == false
+    # while user wants to continue 
+    # execute option menu 
+        puts 'What would you like to do today?'
 
-# options in terminal
-    puts "1. Input a new client into the system"
-    puts "2. View current clients exercise weights information"
-    puts "3. View current clients injury information"
-    puts "4. Update current clients exercise weights amounts"
-    puts "5. Remove a client from the system"
-    puts "6. Exit"
-    user_choice = gets.chomp
-    puts "You selected option #{user_choice}:"
+    # options in terminal
+        print '1. Input a new client into the system'"\n"
+        print '2. View current clients exercise weights information'"\n"
+        print '3. View current clients injury information'"\n"
+        print '4. Update current clients exercise weights amounts'"\n"
+        print '5. Remove a client from the system'"\n"
+        print '6. Exit'
+        user_choice = gets.chomp
+        puts "You selected option #{user_choice}:"
 
-# when user makes selection
-    case user_choice
-    when "1"
-        puts "Input a new client"
-    when "2"
-        puts "View exercise weights info"
-    when "3"
-        puts "View injury info"
-    when "4"
-        puts "Update weights amounts"
-    when "5"
-        puts "Remove a client"
-    when "6"
-        puts "Exit"
-        exit == true
-    end
-    else
-        puts "Please choose a valid option"
+    # when user makes selection
+        case user_choice
+        when "1"
+            puts "Input a new client"
+        when "2"
+            puts "View exercise weights info"
+        when "3"
+            puts "View injury info"
+        when "4"
+            puts "Update weights amounts"
+        when "5"
+            puts "Remove a client"
+        # user doesn't want to continue
+        when "6"
+            puts "Exit"
+            exit = true
+        else
+            puts "Please choose a valid option"
+            system "clear"
+        end
     end
 end
 
