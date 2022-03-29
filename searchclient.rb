845b98aa0af2b91ce9cfd590fa46218a02bce07c
Megan Van Der Weide <meganvanderweide@Megans-MacBook-Air.local>
2022-03-29 15:32:07 +1100

    search for client in the csv file, output row

--- searchclient.rb
+++ searchclient.rb
@@ -2,24 +2,70 @@ require 'csv'
 
 # define functions
 
-def user_search_name(search)
-    print search
+def get_input(message)
+    print message
     print "\n"
-    name_search = gets.chomp
+    user_input = gets.chomp
+  # errors stuff here!! - loop
 end
+
+def user_search(message_to_search)
+    print message_to_search
+    print "\n"
+    search_input = gets.chomp
+end
+
+exit = false
+while exit == false
+
+    puts "Which trainers file would you like to search in? (trainer1, trainer2 or trainer3)?"
+    trainer = gets.chomp
+    puts "You selected #{trainer}"
+
+    case trainer
+    when "trainer1"
+        searchname= user_search('What is the clients last name?')
+        puts "Your answer is #{searchname}"
+        data = CSV.read('trainer1.csv')
+        puts data.find.to_s {|row| row['last_name'] == searchname}
+        p data [1]
+    end
+
+        when "trainer2"
+        searchname= user_search('What is the clients last name?')
+        puts "Your answer is #{searchname}"
+        data = CSV.read('trainer2.csv')
+        puts data.find.to_s {|row| row['last_name'] == searchname}
+        p data [1]
+
+        when "trainer3"
+        searchname= user_search('What is the clients last name?')
+        puts "Your answer is #{searchname}"
+        data = CSV.read('trainer2.csv')
+        puts data.find.to_s {|row| row['last_name'] == searchname}
+        p data [1]
+    end
+
+    when "exit"
+            puts "Exit"
+            exit = true
+            system "clear"
+    else
+            puts "This is not a trainer! Please choose a valid option"
+    end
+end
+
+
 
 # user search name
 
 # user input client name
-name_search = user_search_name('What is the clients last name that you are searching for?')
-puts "Your answer is #{name_search}"
+
 # validate what user typed in
 
 # loop to input question if invalid
 # output message to confirm
 # if no, loop to user input client and exit?
 # output selected clients info
-name_search = CSV.read('trainer1.csv')
-p name_search[1] [2] [3]
 
 # read multiple CSV files???
